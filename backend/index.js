50b585a53b4c3725f4b202441aa359d9c3bae7bc
salmanasif0007 <salmanasif0007@gmail.com>
2021-03-20 10:11:08 +0600

    All

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,10 @@
 
-
-
-
-                        // ---->   index   <----
-
-
-
 var mysql = require('mysql');
 const express = require('express')
 const app = express()
 var cors = require('cors')
 var bodyParser = require('body-parser');
+const initializePassport = require('./server.js')
 
 
 app.use(cors())
@@ -76,6 +70,35 @@ con.connect(function(err) {
 })
 
 
+app.post('/delete',function async(req, res) {
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "inventory"
+    });
+    const data = req.body
+    console.log(data)
+    // con.connect(function(err) {
+    //     if (err) throw err;
+    //     con.query("DELETE FROM inventory WHERE Id=", function (err, result, fields) {
+    //         if (err) throw err;
+    //         console.log('delete');
+    //         // res.send(result)
+    //     });
+    // });
+    con.connect(function(err) {
+        if (err) throw err;
+        var sql = "DELETE FROM inventory WHERE Id= VALUES ";
+        var values = data.id;
+        con.query(sql, values, function (err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    });
+
+})
+
                 // Sales
 
 
@@ -92,13 +115,21 @@ app.get('/sales', function (req, res) {
     // con.connect(function(err) {
     //     if (err) throw err;
     //     console.log("Connected!");
-    //     var sql = "CREATE TABLE sales (Id int PRIMARY KEY NOT NULL AUTO_INCREMENT,PersonID int,FOREIGN KEY (PersonID) REFERENCES inventory(Id),Phone VARCHAR(255) NOT NULL, Name VARCHAR(255), Total int NOT NULL)";
+    //     var sql = "CREATE TABLE sales (Id int PRIMARY KEY NOT NULL AUTO_INCREMENT,PersonID int,FOREIGN KEY (PersonID) REFERENCES inventory(Id),Phone int NOT NULL, Name VARCHAR(255), Total int NOT NULL,CustomerId VARCHAR(255) NOT NULL)";
     //     con.query(sql, function (err, result) {
     //         if (err) throw err;
     //         console.log("Table created");
     //     });
     // });
     
+    // con.connect(function(err) {
+    //     if (err) throw err;
+    //     var sql = "DROP TABLE sales";
+    //     con.query(sql, function (err, result) {
+    //         if (err) throw err;
+    //         console.log("Table deleted");
+    //     });
+    // });
     con.connect(function(err) {
         if (err) throw err;
         con.query("SELECT * FROM sales", function (err, result, fields) {
@@ -113,6 +144,7 @@ app.get('/sales', function (req, res) {
     
     
     app.post('/sales',function async(req, res) {
+
         var con = mysql.createConnection({
             host: "localhost",
             user: "root",
@@ -121,19 +153,59 @@ app.get('/sales', function (req, res) {
         });
         const data = req.body
         console.log(data)
-    con.connect(function(err) {
+        // console.log(salesid)
+    function sl(){
+        var con = mysql.createConnection({
+            host: "localhost",
+            user: "root",
+            password: "password",
+            database: "inventory"
+        });
+        const data = req.body    
+        con.connect(function(err) {
         if (err) throw err;
         console.log("Connected!");
-    
-        var sql = "INSERT INTO sales(PersonID,Phone , Name, Total) VALUES ?";
+        
+        var sql = "INSERT INTO sales(PersonID,Phone , Name, Total,CustomerId) VALUES ?";
         var values = [
-            [data.productId,data.phone,data.name,data.total],
+            [data.productId,data.phone,data.name,data.total,"#BD"+data.phone],
         ];
         con.query(sql, [values], function (err, result) {
             if (err) throw err;
-            console.log(result);
+            console.log(result.insertId);
+            
+        // sa_read();
+        cm(result.insertId);
+
         });
-    });
+    })}
+    
+    function cm(x){
+        var con = mysql.createConnection({
+            host: "localhost",
+            user: "root",
+            password: "password",
+            database: "inventory"
+        });
+        const data = req.body
+        con.connect( function(err) {
+            if (err) throw err;
+            console.log("Connected!");
+           
+        
+        var sql = "INSERT INTO coustomers (SalesID,Name ,Quantity ,Amount , Total) VALUES ?";
+            var values = [
+                [x,"asif",4,21,63],
+            ];
+            con.query(sql, [values], function (err, result) {
+                if (err) throw err;
+                console.log(result);
+            });
+        })
+    }
+    
+        sl();
+        // cm()
     
     })
 
@@ -201,4 +273,57 @@ app.get('/coustomers', function (req, res) {
     
     })
 
+
+
+                    // inventory_details
+
+
+    app.post('/inventory_details',function async(req, res) {
+        let dada;
+        var con = mysql.createConnection({
+            host: "localhost",
+            user: "root",
+            password: "password",
+            database: "inventory"
+        });
+        const data = req.body
+        console.log(data.id)
+       
+        con.connect(function (err) {
+            if (err) throw err;
+            con.query("SELECT * FROM inventory WHERE id = "+ mysql.escape(data.id), function (err, result) {
+              if (err) throw err;
+            //   dada = result;
+                res.send(result)
+            //   console.log(dada)
+            });
+        })
+    })
+
+                        // sales__details
+
+
+app.post('/sales_details',function async(req, res) {
+    
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "inventory"
+    });  
+    const data = req.body
+    console.log(data)
+    
+    con.connect(function (err) {
+        if (err) throw err;
+        con.query("SELECT * FROM inventory WHERE id = "+ mysql.escape(data.id), function (err, result) {
+            if (err) throw err;
+        //   dada = result;
+            res.send(result)
+        //   console.log(dada)
+        });
+        })
+        
+        
+})
 app.listen(8080)
--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,33 @@ const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
 const methodOverride = require('method-override')
+const LocalStrategy = require('passport-local').Strategy
 
-const initializePassport = require('./password-config')
+function initializePassport(passport,getUserByEmail,getUserById){
+    const authenticateUser = async (email, password, done) => {
+        const user = getUserByEmail(email)
+        if (user == null) {
+        return done(null, false, { message: 'No user with that email' })
+        }
+    
+        try {
+        if (await bcrypt.compare(password, user.password)) {
+            return done(null, user)
+        } else {
+            return done(null, false, { message: 'Password incorrect' })
+        }
+        } catch (e) {
+        return done(e)
+        }
+    }
+
+passport.use(new LocalStrategy({usernameField:'email'},
+authenticateUser))
+passport.serializeUser((user,done)=> done(null,user.id) )
+passport.deserializeUser((id,done)=>{ 
+    return done(null,getUserById(id))
+})
+}
 initializePassport(
     passport,
     email => users.find(user => user.email === email),
@@ -43,6 +68,7 @@ app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/login',
     failureFlash: true
+
 }))
 
 app.get('/register', checkNotAuthenticated, (req, res) => {
@@ -84,4 +110,4 @@ function checkNotAuthenticated(req, res, next) {
     next()
 }
 
-app.listen(3000)
+app.listen(8000)
